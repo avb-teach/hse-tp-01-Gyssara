c4e6d4c81405efaec4f7c84856a690e87030cbe7
Gyssara <stoylovg@gmail.com>
2025-04-25 21:23:03 +0300

    поправил файл, теперь работает

--- collect_files.sh
+++ collect_files.sh
@@ -1,23 +1,12 @@
+#!/bin/bash
 if [ "$#" -ne 2 ]; then
-  
+
   exit 1
 fi
 
 input_dir="$1"
 output_dir="$2"
 
-
-generate_unique_filename() {
-  base="$1"
-  ext="$2"
-  dir="$3"
-  count=1
-  newname="${base}${ext}"
-  while [ -e "$dir/$newname" ]; do
-    newname="${base}${count}${ext}"
-    count=$((count + 1))
-  done
-}
 
 find "$input_dir" -type f | while read -r filepath; do
   filename=$(basename "$filepath")
@@ -28,7 +17,5 @@ find "$input_dir" -type f | while read -r filepath; do
   else
     ext=".$ext"
   fi
-  unique_name=$(generate_unique_filename "$name" "$ext" "$output_dir")
   cp "$filepath" "$output_dir/$unique_name"
 done
-
